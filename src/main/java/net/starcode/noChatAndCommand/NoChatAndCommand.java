4d50e1482558d66e686ed5c6c46ffb688dc00358
Hoshii Tomohito <tomo.hoshii47@gmail.com>
2025-05-31 20:55:16 +0900

    second commit

--- src/main/java/net/starcode/noChatAndCommand/NoChatAndCommand.java
+++ src/main/java/net/starcode/noChatAndCommand/NoChatAndCommand.java
@@ -1,17 +1,34 @@
 package net.starcode.noChatAndCommand;
 
+import org.bukkit.event.Listener;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class NoChatAndCommand extends JavaPlugin {
+public final class NoChatAndCommand extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onChat(AsyncPlayerChatEvent event) {
+        if (!event.getPlayer().isOp()) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onCommand(PlayerCommandPreprocessEvent event) {
+        if (!event.getPlayer().isOp()) {
+            event.setCancelled(true);
+        }
     }
 }
